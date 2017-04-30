56ccaa62939a884d4ee6b2e6cc94f7fd0c24cfe9
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2017-04-30 15:44:27 +0200

    Add material-ui

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,45 @@
+import { Component } from 'react'
+
 import { name } from '../package.json'
 import Header from '../components/header'
 import DateTime from '../components/widgets/datetime'
 import PageSpeedScore from '../components/widgets/psi'
 
-export default () => (
-  <div>
-    <Header title={name} />
-    <DateTime />
-    <PageSpeedScore url='https://github.com/' />
-  </div>
-)
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import Head from 'next/head'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  font-family: 'Roboto', sans-serif;
+`
+
+export default class Index extends Component {
+  static getInitialProps ({ req }) {
+    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
+    return { userAgent }
+  }
+
+  componentDidMount () {
+    injectTapEventPlugin()
+  }
+
+  render () {
+    const { userAgent } = this.props
+    const muiTheme = getMuiTheme({ userAgent })
+
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <Container>
+          <Head>
+            <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' rel='stylesheet' />
+          </Head>
+          <Header title={name} />
+          <DateTime />
+          <PageSpeedScore url='https://github.com/' />
+        </Container>
+      </MuiThemeProvider>
+    )
+  }
+}
