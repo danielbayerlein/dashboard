feb0f70355372583f8832f99296746e412044e31
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2017-05-01 11:10:28 +0200

    Add styling for the widget component

--- components/widget.js
+++ components/widget.js
@@ -2,6 +2,11 @@ import { Card, CardTitle, CardText } from 'material-ui/Card'
 import styled from 'styled-components'
 
 const Container = styled(Card)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 20em;
   width: 20em;
 `
 const Title = styled(CardTitle)`
