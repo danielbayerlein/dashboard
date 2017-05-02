ef527ad534074ec79d064b60a4c4b5cf904049a8
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2017-05-02 20:15:37 +0200

    Quick and Dirty replacement for progress-arc-component

--- components/progress.js
+++ components/progress.js
@@ -1,17 +1,20 @@
-import ProgressArc from 'progress-arc-component'
 import styled from 'styled-components'
 import { size } from 'polished'
 
-const Progress = styled(ProgressArc)`
+const Progress = styled.svg`
+  fill: transparent;
   margin: auto;
   ${size('14em')}
 
   circle {
     stroke-width: 10;
-    &.arc {
+    stroke-linecap: round;
+    transform: translate(100px, 100px) rotate(-89.9deg);
+    transition: stroke-dashoffset 0.3s linear;
+    &.progress {
       stroke: ${props => props.theme.palette.primaryColor};
     }
-    &.arc-background {
+    &.background {
       stroke: ${props => props.theme.palette.borderColor};
     }
   }
@@ -19,14 +22,17 @@ const Progress = styled(ProgressArc)`
   text {
     fill: ${props => props.theme.palette.textColor};
     font-size: 4em;
-    font-weight: normal;
-    baseline-shift: -5;
+    text-anchor: middle;
   }
 `
 
+const p = 2 * 90 * Math.PI
+const max = 100
+
 export default ({ value, unit = '' }) => (
-  <Progress value={value}
-    unit={unit}
-    rounded
-  />
+  <Progress viewBox='0 0 200 200'>
+    <circle r='90' className='background' />
+    <circle r='90' className='progress' strokeDasharray={p} strokeDashoffset={((max - value) / max) * p} />
+    <text x='100' y='120'>{value}{unit}</text>
+  </Progress>
 )
