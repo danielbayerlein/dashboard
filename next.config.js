439de694c4c0412ab550727c6547e9272c1d5953
Christopher Helgert <christopher.helgert@witt-gruppe.eu>
2017-05-04 08:26:39 +0200

    Removed UglifyJs filter in webpack.config and solved build-problem with removing universal-url

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // https://github.com/zeit/next.js/issues/1195
-  webpack: (cfg) => {
-    cfg.plugins = cfg.plugins.filter(
-      (plugin) => (plugin.constructor.name !== 'UglifyJsPlugin')
-    )
-    return cfg
-  }
-}
