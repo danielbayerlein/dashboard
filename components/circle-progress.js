68748942d66ea72edcd337b1a18dccfcff90703a
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2017-05-05 15:48:33 +0200

    Own styled components for SVG, Circle and Text

--- components/circle-progress.js
+++ components/circle-progress.js
@@ -2,31 +2,29 @@ import { Component } from 'react'
 import styled from 'styled-components'
 import { size } from 'polished'
 
-const Progress = styled.svg`
+const Svg = styled.svg`
   ${size('14em')}
-
   fill: transparent;
-
-  circle {
-    stroke-linecap: round;
-    stroke-width: 10;
-    transform: translate(100px, 100px) rotate(-89.9deg);
-    transition: stroke-dashoffset 0.3s linear;
-    &.background {
-      stroke: ${props => props.theme.palette.borderColor};
-    }
-    &.progress {
-      stroke: ${props => props.theme.palette.primaryColor};
-    }
-  }
-
   margin: auto;
+`
 
-  text {
-    fill: ${props => props.theme.palette.textColor};
-    font-size: 4em;
-    text-anchor: middle;
+const Circle = styled.circle`
+  stroke-linecap: round;
+  stroke-width: 10;
+  transform: translate(100px, 100px) rotate(-89.9deg);
+  transition: stroke-dashoffset 0.3s linear;
+  &.background {
+    stroke: ${props => props.theme.palette.borderColor};
   }
+  &.progress {
+    stroke: ${props => props.theme.palette.primaryColor};
+  }
+`
+
+const Text = styled.text`
+  fill: ${props => props.theme.palette.textColor};
+  font-size: 4em;
+  text-anchor: middle;
 `
 
 export default class CircleProgress extends Component {
@@ -43,16 +41,16 @@ export default class CircleProgress extends Component {
     const strokeDashoffset = ((max - value) / max) * strokeDasharray
 
     return (
-      <Progress viewBox='0 0 200 200'>
-        <circle r={radius} className='background' />
-        <circle
+      <Svg viewBox='0 0 200 200'>
+        <Circle r={radius} className='background' />
+        <Circle
           r={radius}
           className='progress'
           strokeDasharray={strokeDasharray}
           strokeDashoffset={strokeDashoffset}
         />
-        <text x='100' y='120'>{value}{unit}</text>
-      </Progress>
+        <Text x='100' y='120'>{value}{unit}</Text>
+      </Svg>
     )
   }
 }
