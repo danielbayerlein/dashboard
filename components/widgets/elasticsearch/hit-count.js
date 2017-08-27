19586f38c6c34d861910d2a96887d118853628d5
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2017-08-27 10:26:18 +0200

    Add alert feature to the ElasticsearchHitCount widget

--- components/widgets/elasticsearch/hit-count.js
+++ components/widgets/elasticsearch/hit-count.js
@@ -4,13 +4,18 @@ import yup from 'yup'
 import Widget from '../../widget'
 import Counter from '../../counter'
 import { basicAuthHeader } from '../../../lib/auth'
+import { severity, NONE } from '../../../lib/alert'
 
 const schema = yup.object().shape({
   url: yup.string().url().required(),
   index: yup.string().required(),
   query: yup.string().required(),
   interval: yup.number(),
-  title: yup.string()
+  title: yup.string(),
+  alert: yup.array(yup.object({
+    severity: yup.string().required(),
+    value: yup.number().required()
+  }))
 })
 
 export default class ElasticsearchHitCount extends Component {
@@ -22,7 +27,8 @@ export default class ElasticsearchHitCount extends Component {
   state = {
     count: 0,
     hasError: false,
-    isLoading: true
+    isLoading: true,
+    alertSeverity: NONE
   }
 
   componentDidMount () {
@@ -39,26 +45,33 @@ export default class ElasticsearchHitCount extends Component {
   }
 
   async fetchInformation () {
-    const { authKey, index, query, url } = this.props
+    const { authKey, index, query, url, alert } = this.props
     const opts = authKey ? { headers: basicAuthHeader(authKey) } : {}
 
     try {
       const res = await fetch(`${url}/${index}/_search?q=${query}`, opts)
       const json = await res.json()
+      const total = json.hits.total
 
-      this.setState({ count: json.hits.total, hasError: false, isLoading: false })
+      this.setState({
+        count: total,
+        hasError: false,
+        isLoading: false,
+        alertSeverity: severity(total, alert)
+      })
     } catch (err) {
-      this.setState({ hasError: true, isLoading: false })
+      this.setState({ hasError: true, isLoading: false, alertSeverity: NONE })
     } finally {
       this.interval = setInterval(() => this.fetchInformation(), this.props.interval)
     }
   }
 
   render () {
-    const { count, hasError, isLoading } = this.state
+    const { count, hasError, isLoading, alertSeverity } = this.state
     const { title } = this.props
+
     return (
-      <Widget title={title} isLoading={isLoading} hasError={hasError}>
+      <Widget title={title} isLoading={isLoading} hasError={hasError} alertSeverity={alertSeverity}>
         <Counter value={count} />
       </Widget>
     )
