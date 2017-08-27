f0e88e42900f4d7a38ce86d4fad4e84b7d2921dc
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2017-08-27 10:31:54 +0200

    Add alert feature to the BitbucketPullRequestCount widget

--- components/widgets/bitbucket/pull-request-count.js
+++ components/widgets/bitbucket/pull-request-count.js
@@ -4,6 +4,7 @@ import yup from 'yup'
 import Widget from '../../widget'
 import Counter from '../../counter'
 import { basicAuthHeader } from '../../../lib/auth'
+import { severity, NONE } from '../../../lib/alert'
 
 const schema = yup.object().shape({
   url: yup.string().url().required(),
@@ -12,7 +13,11 @@ const schema = yup.object().shape({
   interval: yup.number(),
   title: yup.string(),
   users: yup.array().of(yup.string()),
-  authKey: yup.string()
+  authKey: yup.string(),
+  alert: yup.array(yup.object({
+    severity: yup.string().required(),
+    value: yup.number().required()
+  }))
 })
 
 export default class BitbucketPullRequestCount extends Component {
@@ -25,7 +30,8 @@ export default class BitbucketPullRequestCount extends Component {
   state = {
     count: 0,
     hasError: false,
-    isLoading: true
+    isLoading: true,
+    alertSeverity: NONE
   }
 
   componentDidMount () {
@@ -42,7 +48,7 @@ export default class BitbucketPullRequestCount extends Component {
   }
 
   async fetchInformation () {
-    const { authKey, url, project, repository, users } = this.props
+    const { authKey, url, project, repository, users, alert } = this.props
     const opts = authKey ? { headers: basicAuthHeader(authKey) } : {}
 
     try {
@@ -56,19 +62,24 @@ export default class BitbucketPullRequestCount extends Component {
         count = json.size
       }
 
-      this.setState({ count, hasError: false, isLoading: false })
+      this.setState({
+        count,
+        hasError: false,
+        isLoading: false,
+        alertSeverity: severity(count, alert)
+      })
     } catch (err) {
-      this.setState({ hasError: true, isLoading: false })
+      this.setState({ hasError: true, isLoading: false, alertSeverity: NONE })
     } finally {
       this.interval = setInterval(() => this.fetchInformation(), this.props.interval)
     }
   }
 
   render () {
-    const { count, hasError, isLoading } = this.state
+    const { count, hasError, isLoading, alertSeverity } = this.state
     const { title } = this.props
     return (
-      <Widget title={title} isLoading={isLoading} hasError={hasError}>
+      <Widget title={title} isLoading={isLoading} hasError={hasError} alertSeverity={alertSeverity}>
         <Counter value={count} />
       </Widget>
     )
