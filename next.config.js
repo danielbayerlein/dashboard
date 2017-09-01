2581b5bed69c7185f5390b35c8b76c5443c445e5
Chris Helgert <chris.helgert@online.de>
2017-09-01 11:46:21 +0200

    use preact via preact-combat in production

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 const Dotenv = require('dotenv-webpack')
 
 module.exports = {
-  webpack: (config) => {
+  webpack: (config, { dev }) => {
     config.plugins.push(
       new Dotenv({ path: './.env' })
     )
+
+    if (!dev) {
+      config.resolve.alias = {
+        react: 'preact-compat/dist/preact-compat',
+        'react-dom': 'preact-compat/dist/preact-compat'
+      }
+    }
+
     return config
   }
 }
