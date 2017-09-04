894d9c9f93d5a47152038237128fc23be4b7b33d
Chris Helgert <chris.helgert@online.de>
2017-09-04 11:16:51 +0200

    add preact on the serverside

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ module.exports = {
       new Dotenv({ path: './.env' })
     )
 
+    // For the development version, we'll use React.
+    // Because, it support react hot loading and so on.
     if (!dev) {
       config.resolve.alias = {
         react: 'preact-compat/dist/preact-compat',
