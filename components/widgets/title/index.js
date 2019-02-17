134448425928d71997881892f2b5b31ff988e4b8
Marco Streng <marcostreng@me.com>
2019-02-17 16:33:32 +0100

    Add Title widget (#116)

---
+++ components/widgets/title/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  flex: 1 1 100%;
+  font-size: 4em;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+`
+
+export default Title
