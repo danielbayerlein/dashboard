8ce42e7f5a0c829f76fd5923df5163a6c0ac96ef
Morten Birkelund <mbi@salecto.com>
2019-07-02 16:44:38 +0200

    Uptimerobot (#10)
    * Initial setup

---
+++ components/monitor-list.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Li = styled.li`
+    min-width: 110px;
+    min-height: 30px;
+    list-style-type: none;
+`
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    min-width: 140px;
+`
+
+export const Circle = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 10px;
+    background: #D50000;
+`
+
+export const Text = styled.div`
+    min-width: 110px;
+    font-weight: 500;
+`
+
+
