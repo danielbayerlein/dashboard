120b79c1f184a3ae2769e80d5bae9b35ec505252
Jay Hack <jayhack@users.noreply.github.com>
2022-11-22 12:13:39 -0800

    [glowup.sh] styled-components => Tailwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      'sans-serif': ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'sans-serif'],
+    },
+  },
+  variants: {},
+  plugins: [],
+}
+```
